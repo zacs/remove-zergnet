b6b2ee88b66c80138537eb70adda328d46432ab2
Zac <zac@z4c.us>
2014-03-08 00:17:25 -0800

    Moved to a RegEx base for removing all elements with 'zerg' in them.

--- remove.zergnet.user.js
+++ remove.zergnet.user.js
@@ -5,8 +5,29 @@
 // @include        http://*/*
 // ==/UserScript==
 
+// RegEx function taken from John K at https://stackoverflow.com/questions/1938294/select-div-using-wildcard-id
+document['getElementsByRegex'] = function(pattern){
+   var arrElements = [];   // to accumulate matching elements
+   var re = new RegExp(pattern);   // the regex to match with
+
+   function findRecursively(aNode) { // recursive function to traverse DOM
+      if (!aNode)
+          return;
+      if (aNode.id !== undefined && aNode.id.search(re) != -1)
+          arrElements.push(aNode);  // FOUND ONE!
+      for (var idx in aNode.childNodes) // search children...
+          findRecursively(aNode.childNodes[idx]);
+   };
+
+   findRecursively(document); // initiate recursive matching
+   return arrElements; // return matching elements
+};
+
 setTimeout(function(){
-    // This previousSibling is _usually_ the "Related Content" text. Doesn't always work though.
-    document.getElementById('zerglayout').parentNode.previousSibling.outerHTML=''
-    document.getElementById('zerglayout').parentNode.outerHTML=''
+    var matches = document.getElementsByRegex('.*zerg.*');
+    var index;
+    for (index = 0; index < matches.length; ++index) {
+        console.log(matches[index]);
+        matches[index].outerHTML='';
+    }
 },2000);
